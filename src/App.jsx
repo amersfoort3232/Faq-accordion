b4fbe6d088ac25126e152975f33080db2f34fe89
Amersfoort Lam <amersfoortlam@gmail.com>
2024-03-10 09:18:40 +0000

    added accessibility by following disclosure ui blog post

--- src/App.jsx
+++ src/App.jsx
@@ -20,45 +20,69 @@ function App() {
           question="What is Frontend Mentor, and how will it help me?"
           answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.
 "
+          airaControlContent="answer1"
         />
         <FAQ
           question="Is Frontend Mentor free?"
           answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+          airaControlContent="answer2"
         />
 
         <FAQ
           question="Can I use Frontend Mentor projects in my portfolio?"
           answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+          airaControlContent="answer3"
         />
 
         <FAQ
           question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
           answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+          airaControlContent="answer4"
         />
       </div>
     </main>
   );
 }
 
-function FAQ({ question, answer }) {
+function FAQ({ question, answer, airaControlContent }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [ariaExpanded, setAriaExpanded] = useState(isOpen);
 
   return (
     <div className="faq-each-container">
       <div className="question-container">
         <h3 className="question">
-          {question} <Icon />
+          {" "}
+          <Icon />
         </h3>
       </div>
-      {isOpen && <p className="answer">{answer}</p>}
+      {isOpen && (
+        <p className="answer" id={airaControlContent}>
+          {answer}
+        </p>
+      )}
     </div>
   );
 
   function Icon() {
     return (
-      <span className="icon-container" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <MinusIcon /> : <PlusIcon />}
-      </span>
+      <button
+        aria-expanded={ariaExpanded}
+        aria-controls={airaControlContent}
+        className="icon-container"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {question}
+        {isOpen ? (
+          <MinusIcon
+            className="minusIcon"
+            ocusable="false"
+            aria-hidden="true"
+          />
+        ) : (
+          <PlusIcon className="plusIcon" focusable="false" aria-hidden="true" />
+        )}
+      </button>
     );
   }
 }
