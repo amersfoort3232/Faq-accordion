31859fae32e553383c901d6ce576945050507159
Amersfoort Lam <amersfoortlam@gmail.com>
2024-03-11 21:28:06 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,31 +15,29 @@ function App() {
           FAQs
         </h1>
       </div>
-      <div className="question-container">
-        <FAQ
-          question="What is Frontend Mentor, and how will it help me?"
-          answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.
+      <FAQ
+        question="What is Frontend Mentor, and how will it help me?"
+        answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.
 "
-          airaControlContent="answer1"
-        />
-        <FAQ
-          question="Is Frontend Mentor free?"
-          answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
-          airaControlContent="answer2"
-        />
+        airaControlContent="answer1"
+      />
+      <FAQ
+        question="Is Frontend Mentor free?"
+        answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+        airaControlContent="answer2"
+      />
 
-        <FAQ
-          question="Can I use Frontend Mentor projects in my portfolio?"
-          answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
-          airaControlContent="answer3"
-        />
+      <FAQ
+        question="Can I use Frontend Mentor projects in my portfolio?"
+        answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+        airaControlContent="answer3"
+      />
 
-        <FAQ
-          question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
-          answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
-          airaControlContent="answer4"
-        />
-      </div>
+      <FAQ
+        question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
+        answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+        airaControlContent="answer4"
+      />
     </main>
   );
 }
@@ -51,15 +49,11 @@ function FAQ({ question, answer, airaControlContent }) {
   return (
     <div className="faq-each-container">
       <div className="question-container">
-        <div className="question">
-          <Icon />
-        </div>
+        <Icon />
       </div>
-      {isOpen && (
-        <p className="answer" id={airaControlContent}>
-          {answer}
-        </p>
-      )}
+      <div className="answer-container" id={airaControlContent}>
+        {isOpen && <p className="answer">{answer}</p>}
+      </div>
     </div>
   );
 
@@ -68,10 +62,10 @@ function FAQ({ question, answer, airaControlContent }) {
       <button
         aria-expanded={ariaExpanded}
         aria-controls={airaControlContent}
-        className="icon-container"
+        className="button-container"
         onClick={() => setIsOpen(!isOpen)}
       >
-        {question}
+        <div className="question">{question}</div>
         {isOpen ? (
           <MinusIcon
             className="minusIcon"
